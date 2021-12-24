5d8c31da4e4354710495db699620364ee54b0262
Jean Ferreira <jeanferreira.es@gmail.com>
2021-12-24 18:19:05 -0400

    Adicionado as configurações de navegação do app

---
+++ mobile/src/pages/HomeScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function index() {
+    return (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{fontSize: 100, color: '#000'}}>Home Screen</Text>
+        </View>
+    )
+}
